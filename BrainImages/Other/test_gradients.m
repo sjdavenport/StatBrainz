59f7cefdc6053877844c52044a984dc9012c84d6
Samuel Davenport <12sdavenport@gmail.com>
2026-07-07 21:52:12 +0200

    backup

--- BrainImages/Other/test_gradients.m
+++ BrainImages/Other/test_gradients.m
@@ -13,10 +13,10 @@ xml = xmlread('/Users/samd/Documents/Other/MyCode/StatBrainz/BrainImages/Gifti_f
 gii = read_gifti('/Users/samd/Documents/Other/MyCode/StatBrainz/BrainImages/Gifti_files/S1200.L.inflated_MSMAll.32k_fs_LR.surf.gii');
 
 %%
-gifti2srf('/Users/samd/Documents/Other/MyCode/StatBrainz/BrainImages/Surface/hcp/Sphere.10k.L.surf.gii', '/Users/samd/Documents/Other/MyCode/StatBrainz/BrainImages/Surface/hcp/Sphere.10k.R.surf.gii')
+gifti2surf('/Users/samd/Documents/Other/MyCode/StatBrainz/BrainImages/Surface/hcp/Sphere.10k.L.surf.gii', '/Users/samd/Documents/Other/MyCode/StatBrainz/BrainImages/Surface/hcp/Sphere.10k.R.surf.gii')
 
 %%
-gifti('/Users/samd/Documents/Other/MyCode/StatBrainz/BrainImages/Surface/hcp/Sphere.10k.L.surf.gii')
+read_gifti('/Users/samd/Documents/Other/MyCode/StatBrainz/BrainImages/Surface/hcp/Sphere.10k.L.surf.gii')
 
 %%
 lh = load('/Users/samd/Documents/Other/MyCode/StatBrainz/BrainImages/Surface/hcp/Sphere.10k.L.surf.mat')
